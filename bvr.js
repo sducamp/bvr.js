db64f0c2ddf3a8c5f35237bdea72eb1396605ee7
Sylvain Ducamp <sylvain@akufen.ca>
2015-10-14 09:13:19 +0200

    Add the jovascript file

---
+++ bvr.js
@@ -0,0 +1,48 @@
+/*jslint browser: true, white: true */
+/*global define, require */
+
+define([], function () {
+
+    // Table from https://www.credit-suisse.com/media/production/pb/docs/unternehmen/kmugrossunternehmen/besr_technische_dokumentation_fr.pdf
+    var table = [
+        [0,9,4,6,8,2,7,1,3,5],
+        [9,4,6,8,2,7,1,3,5,0],
+        [4,6,8,2,7,1,3,5,0,9],
+        [6,8,2,7,1,3,5,0,9,4],
+        [8,2,7,1,3,5,0,9,4,6],
+        [2,7,1,3,5,0,9,4,6,8],
+        [7,1,3,5,0,9,4,6,8,2],
+        [1,3,5,0,9,4,6,8,2,7],
+        [3,5,0,9,4,6,8,2,7,1],
+        [5,0,9,4,6,8,2,7,1,3]
+      ],
+    maxLength = 26, // The maximun length for the reference number on base 0
+    report = 0, // The default report number
+
+    bvr = {
+
+        /**
+         * Get the key number for the given reference number
+         *
+         * @param {string} refNumber The reference number without the key number (26 characters)
+         * @return {int} The valid key number for the current reference number
+         */
+        getKey: function(refNumber) {
+            // Reset the report
+            report = 0;
+
+            // Remove the white space in the reference number and convert it to array
+            refNumber = refNumber.split(" ").join("").split("");
+
+            // Get the report number
+            for (var i=0; i < refNumber.length && i < maxLength; i++) {
+                report = table[report][refNumber[i]];
+            }
+
+            // Return the validation key for a mod-10
+            return 10-report;
+        }
+    };
+
+    return bvr;
+});
